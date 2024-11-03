2fd936c986d8c69825c0adf825a6efde747c1d2b
Noah <bigspaceships56@gmail.com>
2024-11-03 15:06:54 -0500

    FEAT: basic camera

---
+++ Assets/Editor/AdventuringCameraEditor.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(AdventuringCamera))]
+public class AdventuringCameraEditor : Editor {
+    public override VisualElement CreateInspectorGUI() {
+        var baseElement = new VisualElement();
+
+        InspectorElement.FillDefaultInspector(baseElement, serializedObject, this);
+
+        var pictureButton = new Button { text = "Take Picture" };
+
+        pictureButton.clicked += () => {
+            ((AdventuringCamera)target).TakePicture();
+        };
+
+        baseElement.Add(pictureButton);
+
+        return baseElement;
+    }
+}
---
+++ Assets/Script/AdventuringCamera.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventuringCamera : MonoBehaviour {
+    [SerializeField] private Camera displayCamera;
+    [SerializeField] private RawImage screenImage;
+
+    [SerializeField] private Vector2Int displayResolution;
+
+    private RenderTexture _displayRenderTexture;
+
+    public List<RenderTexture> takenPictures;
+
+    private void Start() {
+        takenPictures = new List<RenderTexture>();
+
+        _displayRenderTexture =
+            new RenderTexture(displayResolution.x, displayResolution.y, 0, RenderTextureFormat.ARGB32);
+
+        screenImage.texture = _displayRenderTexture;
+
+        displayCamera.targetTexture = _displayRenderTexture;
+    }
+
+    public void TakePicture() {
+        Debug.Log("taking picture");
+        var newTexture = new RenderTexture(_displayRenderTexture);
+        displayCamera.targetTexture = newTexture;
+        displayCamera.Render();
+
+        takenPictures.Add(newTexture);
+
+        displayCamera.targetTexture = _displayRenderTexture;
+    }
+}
