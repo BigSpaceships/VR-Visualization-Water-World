609ad025e288909b3bb3f2fbe8bd5aa3b89ab551
Noah <bigspaceships56@gmail.com>
2024-11-14 11:05:46 -0500

    FEAT: camera turns while moving

--- Assets/Scripts/SeaScooter.cs
+++ Assets/Scripts/SeaScooter.cs
@@ -1,16 +1,17 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class SeaScooter : ActionBasedContinuousMoveProvider {
-    [SerializeField] protected Transform rightHandTransform;
+public class SeaScooter : LocomotionProvider {
+    [SerializeField] private Transform rightHandTransform;
+    [SerializeField] private Transform cameraTransform;
 
 
-    [SerializeField]
-    [Tooltip(
-        "The Input System Action that will be used to read Move data from the right hand controller. Must be a Value Vector2 Control.")]
-    protected InputActionProperty rightHandMoveAction =
-        new InputActionProperty(new InputAction("Right Hand Move", expectedControlType: "Vector2"));
+    [SerializeField] private InputActionProperty rightHandMoveAction =
+        new(new InputAction("Right Hand Move", expectedControlType: "Vector2"));
+
+    private CharacterController _characterController;
 
     private float _timeAccelerating = 0;
 
@@ -18,24 +19,62 @@ public class SeaScooter : ActionBasedContinuousMoveProvider {
     [SerializeField] private float maxSpeed;
     [SerializeField] private float accelerationRate;
 
+    [SerializeField] private float sidewaysSpeed;
+    [SerializeField] private float turnRateFromInput;
+    [SerializeField] private float turnRateCorrective;
+
+    private float _moveSpeed;
+
     [SerializeField] private float backSpeed;
 
     protected override void Awake() {
         base.Awake();
 
-        forwardSource = rightHandTransform;
+        if (!_characterController) {
+            var xrOrigin = system.xrOrigin?.Origin;
+            if (xrOrigin != null) {
+                xrOrigin.TryGetComponent(out _characterController);
+            }
+        }
     }
 
-    protected override Vector3 ComputeDesiredMove(Vector2 input) {
+    private void Update() {
+        var relativeMovement = GetMoveFromInput(rightHandMoveAction.action.ReadValue<Vector2>()) * Time.deltaTime;
+
+        var motion = rightHandTransform.right * relativeMovement.x + rightHandTransform.forward * relativeMovement.y;
+
+        var projectedCameraForward = Vector3.ProjectOnPlane(cameraTransform.forward, Vector3.up);
+        var projectedControllerForward = Vector3.ProjectOnPlane(rightHandTransform.forward, Vector3.up);
+
+        var lookRot = Quaternion.FromToRotation(projectedCameraForward, projectedControllerForward);
+
+        var lookTurnAmount = Mathf.Min(lookRot.eulerAngles.y, 360 - lookRot.eulerAngles.y) *
+                             Mathf.Sign(180 - lookRot.eulerAngles.y) * Time.deltaTime;
+        // TODO: use an easing function for this because it can get a bit jarring
+
+        var turnAmount = relativeMovement.x * turnRateFromInput + lookTurnAmount * turnRateCorrective;
+
+        if (relativeMovement != Vector2.zero) {
+            if (CanBeginLocomotion() && BeginLocomotion()) {
+                _characterController.Move(motion);
+
+                system.xrOrigin.RotateAroundCameraUsingOriginUp(turnAmount);
+
+                EndLocomotion();
+            }
+        }
+    }
+
+    private Vector2 GetMoveFromInput(Vector2 input) {
         if (input == Vector2.zero)
             return Vector3.zero;
 
         input = rightHandMoveAction.action?.ReadValue<Vector2>() ?? Vector2.zero;
 
-        input.x = 0; // TODO: have this turn it a bit
+        input.x *= sidewaysSpeed;
 
         if (input.y < 0) {
-            moveSpeed = backSpeed;
+            _moveSpeed = backSpeed;
 
             _timeAccelerating = 0;
         }
@@ -47,11 +86,12 @@ public class SeaScooter : ActionBasedContinuousMoveProvider {
         if (input.y > 0) {
             _timeAccelerating += Time.deltaTime;
 
-            moveSpeed = CalculateSpeed();
+            _moveSpeed = CalculateSpeed();
         }
 
+        input.y *= _moveSpeed;
 
-        return base.ComputeDesiredMove(input);
+        return input;
     }
 
     private float CalculateSpeed() {
